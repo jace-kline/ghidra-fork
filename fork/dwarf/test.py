1cf5dde99267c95e3b116a7a980369707f69f2ef
Jace Kline <jace.a.kline@gmail.com>
2022-06-27 14:37:34 -0500

    Playing around with pyelftools library for extracting DWARF info from obj files

---
+++ fork/dwarf/test.py
@@ -0,0 +1,50 @@
+from elftools.common.py3compat import maxint, bytes2str
+from elftools.dwarf.descriptions import describe_form_class
+from elftools.elf.elffile import ELFFile
+
+def mk_ELFFile(fname):
+    try:
+        f = open(fname, 'rb')
+        elffile = ELFFile(f)
+        return elffile
+    except:
+        return None
+
+# get all DIE entries across all CUs
+# ignore null DIEs
+def get_all_DIEs(dwarfinfo):
+    dies = []
+    for cu in dwarfinfo.iter_CUs():
+        for die in cu.iter_DIEs():
+            if not die.is_null():
+                dies.append(die)
+    return dies
+
+def main():
+    objfilepath = "./progs/p0"
+    elffile = mk_ELFFile(objfilepath)
+    if elffile is None:
+        print("Error: Could not parse ELF info")
+        return
+
+    if not elffile.has_dwarf_info():
+        print("Error: File has no DWARF info")
+        return
+
+    dwarfinfo = elffile.get_dwarf_info()
+
+    if not dwarfinfo.has_debug_info:
+        print("Error: DWARF info has no debug info")
+        return
+
+    # get all DIE objects across all CUs
+    dies = get_all_DIEs(dwarfinfo)
+    for die in dies:
+        print(die)
+
+    
+    elffile.stream.close()
+
+if __name__ == "__main__":
+    main()
+    
